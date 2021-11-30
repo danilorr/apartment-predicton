dfb7347fe4adb98688f80e270362bc42a0a4b0b8
Danilo Rosmaninho <danilorosmaninho@gmail.com>
2021-11-30 09:37:37 -0300

    making forms on the web app

--- web_app.py
+++ web_app.py
@@ -38,7 +38,7 @@ class WebApp:
     def title(self):
         st.write("""
         
-        # Calculador de preço de aluguel de apartamento em São Paulo
+        # Calculadora de preço de aluguel de apartamento em São Paulo
         
         
         A calculadora a seguir usa informações coletadas de 10 mil anúncios de aluguel na cidade de São Paulo, de Novembro de
@@ -49,29 +49,33 @@ class WebApp:
         st.write("#")
 
     def user_input_features(self):
-        st.subheader('Características do apartamento')
+        with st.form(key='my-form'):
 
-        district = st.selectbox(
-            "Escolha o Bairro em que seu apartamento se encontra.\n Caso o seu Bairro não se encontre na lista, "
-            "escolha a opção 'Outro'",
-            ('Outro', 'Aclimação', 'Alto Da Boa Vista', 'Alto da Lapa', 'Alto de Pinheiros', 'Barra Funda', 'Bela Vista',
-             'Bom Retiro', 'Brooklin', 'Brás', 'Butantã', 'Cambuci', 'Campo Belo', 'Campos Eliseos', 'Casa Verde',
-             'Centro', 'Cerqueira César', 'Chácara Inglesa', 'Chácara Klabin', 'Chácara Santo Antônio',
-             'Cidade Monções', 'Consolação', 'Freguesia do Ó', 'Granja Julieta', 'Higienópolis', 'Indianópolis',
-             'Ipiranga', 'Itaim Bibi', 'Jabaquara', 'Jaguaré', 'Jardim América', 'Jardim Anália Franco',
-             'Jardim Europa', 'Jardim Marajoara', 'Jardim Paulista', 'Jardim Prudência', 'Jardins', 'Lapa', 'Liberdade',
-             'Mirandópolis', 'Moema', 'Morumbi', 'Móoca', 'Panamby', 'Paraíso', 'Perdizes', 'Pinheiros',
-             'Pirituba', 'Pompeia', 'República', 'Santa Cecília', 'Santa Ifigênia', 'Santana', 'Santo Amaro', 'Saúde',
-             'Sumaré', 'Tatuapé', 'Vila Anastácio', 'Vila Andrade', 'Vila Buarque', 'Vila Carrão', 'Vila Clementino',
-             'Vila Cruzeiro', 'Vila Formosa', 'Vila Gomes Cardim', 'Vila Guarani', 'Vila Leopoldina', 'Vila Madalena',
-             'Vila Mariana', 'Vila Mascote', 'Vila Nova Conceição', 'Vila Olímpia', 'Vila Prudente', 'Vila Romana',
-             'Vila Santa Catarina', 'Vila Sônia', 'Vila Uberabinha', 'Água Branca')
-        )
-        area = st.number_input('Insira a ÁREA do apartamento (em m²)', min_value=10, step=1)
-        bedrooms = st.number_input('Insira o NÚMERO DE QUARTOS do apartamento', min_value=1, step=1)
-        bathrooms = st.number_input('Insira o NÚMERO DE BANHEIROS do apartamento', min_value=1, step=1)
-        garage_cars = st.number_input('Insira o NÚMERO DE VAGAS DE GARAGEM do apartamento', min_value=0, step=1)
-        cond_fee = st.number_input('Insira o VALOR DO CONDOMÍNIO do apartamento (em R$)', min_value=0, step=1)
+            st.subheader('Características do apartamento')
+
+            district = st.selectbox(
+                "Escolha o Bairro em que seu apartamento se encontra.\n Caso o seu Bairro não se encontre na lista, "
+                "escolha a opção 'Outro'",
+                ('Outro', 'Aclimação', 'Alto Da Boa Vista', 'Alto da Lapa', 'Alto de Pinheiros', 'Barra Funda', 'Bela Vista',
+                 'Bom Retiro', 'Brooklin', 'Brás', 'Butantã', 'Cambuci', 'Campo Belo', 'Campos Eliseos', 'Casa Verde',
+                 'Centro', 'Cerqueira César', 'Chácara Inglesa', 'Chácara Klabin', 'Chácara Santo Antônio',
+                 'Cidade Monções', 'Consolação', 'Freguesia do Ó', 'Granja Julieta', 'Higienópolis', 'Indianópolis',
+                 'Ipiranga', 'Itaim Bibi', 'Jabaquara', 'Jaguaré', 'Jardim América', 'Jardim Anália Franco',
+                 'Jardim Europa', 'Jardim Marajoara', 'Jardim Paulista', 'Jardim Prudência', 'Jardins', 'Lapa', 'Liberdade',
+                 'Mirandópolis', 'Moema', 'Morumbi', 'Móoca', 'Panamby', 'Paraíso', 'Perdizes', 'Pinheiros',
+                 'Pirituba', 'Pompeia', 'República', 'Santa Cecília', 'Santa Ifigênia', 'Santana', 'Santo Amaro', 'Saúde',
+                 'Sumaré', 'Tatuapé', 'Vila Anastácio', 'Vila Andrade', 'Vila Buarque', 'Vila Carrão', 'Vila Clementino',
+                 'Vila Cruzeiro', 'Vila Formosa', 'Vila Gomes Cardim', 'Vila Guarani', 'Vila Leopoldina', 'Vila Madalena',
+                 'Vila Mariana', 'Vila Mascote', 'Vila Nova Conceição', 'Vila Olímpia', 'Vila Prudente', 'Vila Romana',
+                 'Vila Santa Catarina', 'Vila Sônia', 'Vila Uberabinha', 'Água Branca')
+            )
+            area = st.number_input('Insira a ÁREA do apartamento (em m²)', min_value=10, step=1)
+            bedrooms = st.number_input('Insira o NÚMERO DE QUARTOS do apartamento', min_value=1, step=1)
+            bathrooms = st.number_input('Insira o NÚMERO DE BANHEIROS do apartamento', min_value=1, step=1)
+            garage_cars = st.number_input('Insira o NÚMERO DE VAGAS DE GARAGEM do apartamento', min_value=0, step=1)
+            cond_fee = st.number_input('Insira o VALOR DO CONDOMÍNIO do apartamento (em R$)', min_value=0, step=1)
+
+            st.form_submit_button('Calcular')
 
         data = {'District': district,
                 'Area (m²)': area,
@@ -80,6 +84,7 @@ class WebApp:
                 'Garage Cars': garage_cars,
                 'Condominium Fee (R$)': cond_fee}
         self.features_df = pd.DataFrame(data, index=[0])
+
 
     def create_predict_df(self):
         self.predict_df = self.features_df.drop(['Bedrooms', 'Bathrooms', 'Garage Cars'], axis=1)
@@ -90,7 +95,7 @@ class WebApp:
         self.prediction = self.model.forest.predict(self.predict_df)
         self.prediction = np.around(self.prediction, 2)
         st.write("#")
-        st.write(f'O valor estimado do apartamento é: {self.prediction.item(0)}')
+        st.write(f'O valor estimado do apartamento é: **{self.prediction.item(0)}**')
 
 
 
